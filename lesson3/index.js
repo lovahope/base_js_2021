48cdc58e438f3efa06b9c09ebe26a0df98faff84
lovahope <82157564+lovahope@users.noreply.github.com>
2021-10-09 09:30:14 +0300

    lesson4

---
+++ lesson3/index.js
@@ -0,0 +1,92 @@
+// const firstValue = String(3);
+
+// console.log(typeof firstValue);
+// const obj = {
+//   foo: 67,
+// };
+// console.log(typeof String(obj));
+// console.log("==========================================================");
+// let testValue;
+// console.log(typeof testValue);
+
+// // testValue = Nubmer(testValue);
+// // console.log(testValue);
+// console.log("==========================================================");
+
+// const isUser = true;
+// const isAdmin = false;
+
+// console.log(Number(isUser), Number(isAdmin));
+// const voidString = "";
+// console.log(Number(voidString));
+// console.log("==========================================================");
+// const value = 0;
+// console.log(typeof Boolean(value), Boolean(value));
+// console.log("==========================================================");
+
+// const obj = [1].length;
+// console.log(Boolean(obj));
+
+// Математические операторы
+// console.log(16 % 2);
+// console.log(16 % 5);
+// унарный плюс
+// const value = 5;
+// console.log(typeof value, typeof +value);
+// const value = "5";
+// console.log(typeof value, typeof +value, typeof Number(value));
+// Бинарный плюс
+// const sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = "12";
+// const numberValue = 34;
+
+// console.log(+stringValue + numberValue);
+// console.log(typeof (numberValue + +stringValue));
+
+// const firstName = "Alex";
+// const lastName = "Shiricov";
+// console.log(firstName + " " + lastName);
+// console.log(`${firstName} ${lastName}`);
+
+// const firstName1 = "Andrey";
+// const lastName1 = "Sereda";
+// console.log(`My name ${firstName1} and last ${lastName1}`);
+
+// Операторы сравнения
+
+// console.log(false == 0);
+
+// const firstString = "яблоко";
+// const secondString = "Яблок0";
+
+// console.log(firstString != secondString);
+
+// Оператор строгого равенства
+
+// console.log(a === b);
+
+// Примеры
+// console.log("" + 1 + 0);
+// console.log(typeof ("" + 1 + 0));
+
+// console.log("" - 1 + 0);
+// console.log(typeof ("" - 1 + 0));
+
+// console.log(true + false);
+
+// console.log(6 / "3");
+// console.log(typeof (6 / "3"));
+
+// console.log("2" * "3");
+// console.log(typeof ("2" * "3"));
+
+// console.log(" -9 " - 5);
+// console.log(null + 1);
---
+++ lesson4/index.js
@@ -0,0 +1,14 @@
+// if (true) {
+//   console.log("TRUE");
+// }
+// if (0) {
+//   console.log("false");
+// }
+// console.log("start");
+// const value = -5;
+// if (value > 0) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// console.log("finish");
