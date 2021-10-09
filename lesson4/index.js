03f351cdbb5ee952def5bcb02e8a8ce318903d2c
lovahope <82157564+lovahope@users.noreply.github.com>
2021-10-09 09:55:52 +0300

    дополнение

--- lesson4/index.js
+++ lesson4/index.js
@@ -12,3 +12,18 @@
 //   console.log("false");
 // }
 // console.log("finish");
+// console.log("start");
+// const value = 0;
+// if(value === 0){
+//     console.log(value, "=value");
+// }else if(value>1){
+//     console.log(value,">1");
+// }else if(valu<-1);{
+//      console.log(value," <-1");
+//  }else{
+//      console.log(value);
+// }
+//  console.log(finish);
+// const age = 20;
+// const result = age >= 18 ? "Доступ разрешен" : "Доступ запрещен";
+// console.log(result);
