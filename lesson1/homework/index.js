cac82d7736899036a46f5b59b61c6d9babcdc480
lovahope <82157564+lovahope@users.noreply.github.com>
2021-10-11 20:21:05 +0300

    доп

--- lesson1/homework/index.js
+++ lesson1/homework/index.js
@@ -1,10 +1,11 @@
-// console.log("hello world");
+console.log("hello world");
 
-let num = 123; //number
+var num = 123; //number
+num = "123"; //Изменение переменной
 console.log(typeof num);
 
 const userName = "NA"; //string
-userName = String; //изменение const
+userName = 345; //Изменение переменной
 console.log(typeof userName);
 
 let isUser = false; //boolean
@@ -17,12 +18,12 @@ let X; //undefined
 console.log(typeof X);
 
 let newVisitor = {
-  //object
-  name: "Brand",
+  name: "Brand", //object
   age: 30,
   bloodType: 2,
 };
 console.log(typeof newVisitor);
+newVisitor.name = 13; //Изменение переменной
 
 x = Symbol("color"); //symbol
 console.log(typeof x);
